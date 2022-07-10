9d26bd79b00467fd64ad4379e889d345c1c5ece9
Matt Gaunt <matt@gaunt.dev>
2022-07-10 10:40:42 -0700

    Remove tracing (#42)

--- src/scripts/utils/_sentry.ts
+++ src/scripts/utils/_sentry.ts
@@ -1,10 +1,9 @@
 import * as Sentry from '@sentry/browser';
-import {BrowserTracing} from '@sentry/tracing';
 import * as browser from 'webextension-polyfill';
 
 Sentry.init({
   dsn: 'https://0b9cdcee8d8040edb9cc9586ecb52a14@o1296550.ingest.sentry.io/6556279',
-  integrations: [new BrowserTracing()],
+  integrations: [],
   release: `pin-it-extension@${browser.runtime.getManifest().version}`,
   tracesSampleRate: 0.1,
 });
