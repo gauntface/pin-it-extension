94c6960d552672d03fbe9100730c2741059f9fd7
Matt Gaunt-Seo <matt@gaunt.dev>
2024-08-04 12:56:25 -0700

    Update eslint (#517)

---
+++ eslint.config.js
@@ -0,0 +1,52 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintPluginSvelte from "eslint-plugin-svelte";
+import eslintConfigPrettier from "eslint-config-prettier";
+import globals from "globals";
+import svelteParser from "svelte-eslint-parser";
+
+export default tseslint.config(
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...eslintPluginSvelte.configs["flat/recommended"],
+  eslintConfigPrettier,
+  ...eslintPluginSvelte.configs["flat/prettier"],
+  {
+    files: ["**/*.svelte"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: { ...globals.node, ...globals.browser },
+      parser: svelteParser,
+      parserOptions: {
+        parser: tseslint.parser,
+        extraFileExtensions: [".svelte"],
+      },
+    },
+  },
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      parser: tseslint.parser,
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          args: "all",
+          argsIgnorePattern: "^_",
+          caughtErrors: "all",
+          caughtErrorsIgnorePattern: "^_",
+          destructuredArrayIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          ignoreRestSiblings: true,
+        },
+      ],
+    },
+  },
+);
