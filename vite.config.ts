05bb8fe71104dcf89a0d964c9c874f081b295603
Matt Gaunt-Seo <hello@gaunt.dev>
2025-11-17 20:26:42 -0800

    Update for firefox build

--- vite.config.ts
+++ vite.config.ts
@@ -112,6 +112,70 @@ async function bundleExtension() {
   }
 }
 
+async function bundleExtensionSrc() {
+  console.log("Bundling src zip");
+  const zipPath = join(resolve(__dirname), "gauntface-pin-it-src.zip");
+  try {
+    await rm(zipPath);
+  } catch (e) {
+    // NOOP
+  }
+
+  const output = createWriteStream(zipPath);
+  // eslint-disable-next-line new-cap
+  const archive = archiver("zip", {
+    zlib: {
+      // Sets the compression level
+      level: 9,
+    },
+  });
+
+  const srcDir = resolve(__dirname, "src");
+
+  try {
+    await new Promise((resolve, reject) => {
+      output.on("close", () => {
+        resolve(null);
+      });
+
+      output.on("end", () => {
+        console.log("Output End event");
+      });
+
+      // good practice to catch warnings (ie stat failures and other
+      // non-blocking errors)
+      archive.on("warning", (err) => {
+        if (err.code === "ENOENT") {
+          console.warn(`Archiver warning: ${err.message}`);
+        } else {
+          reject(err);
+        }
+      });
+
+      // good practice to catch this error explicitly
+      archive.on("error", (err) => {
+        reject(err);
+      });
+
+      // pipe archive data to the file
+      archive.pipe(output);
+
+      // append files from a sub-directory and naming it `gauntface-pin-it-extension`
+      // within the archive
+      archive.directory(srcDir, false);
+
+      // finalize the archive (ie we are done appending files but streams have to
+      // finish yet)
+      // 'close', 'end' or 'finish' may be fired right after calling this method
+      // so register to them beforehand
+      archive.finalize();
+    });
+  } catch (err) {
+    console.error(`Error while zipping: ${err}`);
+    throw err;
+  }
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
@@ -138,6 +202,7 @@ export default defineConfig({
         await writeManifest();
         await injectSentryDebugIDs();
         await bundleExtension();
+        await bundleExtensionSrc();
       },
     },
   ],
